bdca45f456f1419794c4289a49f0a8f853955110
Fred Block <info@fredblock.com>
2015-08-12 02:48:44 -0600

    Update 'features/user_can_show_list_spec.rb'.

--- spec/features/user_can_show_list_spec.rb
+++ spec/features/user_can_show_list_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "show list" do
 
-  # it "can show a list" do
-  #   visit list_path
-  #
-  #   expect(current_path).to eq list_path
-  # end
+  it "can show a list" do
+    visit root_path
+    click_link_or_button "Create New List"
+    fill_in 'Title', with: "write code"
+    fill_in 'Description', with: "better, faster, stronger"
+    click_link_or_button "Submit"
+
+    expect(page).to have_content("write code")
+  end
 
 end
